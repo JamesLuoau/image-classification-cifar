d39f9087d3b615fb291ebe09781b5439dc47d1f2
James Luo <jluo@bglcorp.com.au>
2017-05-15 22:18:40 +1000

    reaches to first CONV layer

--- image_classification.py
+++ image_classification.py
@@ -26,3 +26,73 @@ def one_hot_encode(x):
     return a
 
 tests.test_one_hot_encode(one_hot_encode)
+
+import tensorflow as tf
+import pickle
+import problem_unittests as tests
+import helper
+
+# Load the Preprocessed Validation data
+valid_features, valid_labels = pickle.load(open('preprocess_validation.p', mode='rb'))
+
+
+def neural_net_image_input(image_shape):
+    """
+    Return a Tensor for a batch of image input
+    : image_shape: Shape of the images
+    : return: Tensor for image input.
+    """
+    return tf.placeholder(tf.uint8, (None, *image_shape), "x")
+
+
+def neural_net_label_input(n_classes):
+    """
+    Return a Tensor for a batch of label input
+    : n_classes: Number of classes
+    : return: Tensor for label input.
+    """
+    return tf.placeholder(tf.uint8, (None, n_classes), "y")
+
+
+def neural_net_keep_prob_input():
+    """
+    Return a Tensor for keep probability
+    : return: Tensor for keep probability.
+    """
+    return tf.placeholder(tf.float16, None, "keep_prob")
+
+
+"""
+DON'T MODIFY ANYTHING IN THIS CELL THAT IS BELOW THIS LINE
+"""
+tf.reset_default_graph()
+tests.test_nn_image_inputs(neural_net_image_input)
+tests.test_nn_label_inputs(neural_net_label_input)
+tests.test_nn_keep_prob_inputs(neural_net_keep_prob_input)
+
+def conv2d_maxpool(x_tensor, conv_num_outputs, conv_ksize, conv_strides, pool_ksize, pool_strides):
+    """
+    Apply convolution then max pooling to x_tensor
+    :param x_tensor: TensorFlow Tensor
+    :param conv_num_outputs: Number of outputs for the convolutional layer
+    :param conv_ksize: kernal size 2-D Tuple for the convolutional layer
+    :param conv_strides: Stride 2-D Tuple for convolution
+    :param pool_ksize: kernal size 2-D Tuple for pool
+    :param pool_strides: Stride 2-D Tuple for pool
+    : return: A tensor that represents convolution and max pooling of x_tensor
+    """
+    # TODO: Implement
+    conv1_W = tf.Variable(tf.truncated_normal(
+        shape=(conv_ksize[0], conv_ksize[1], x_tensor.shape[3].value, conv_num_outputs)))
+    conv1_b = tf.Variable(tf.zeros(conv_num_outputs))
+    conv1 = tf.nn.conv2d(x_tensor, filter=conv1_W, strides=[1, conv_strides[0], conv_strides[1], 1], padding="SAME") + conv1_b
+    conv1 = tf.nn.relu(conv1)
+    conv1 = tf.nn.max_pool(conv1, ksize=[1, pool_ksize[0], pool_ksize[1], 1],
+                           strides=[1, pool_strides[0], pool_strides[1], 1], padding='SAME')
+    return conv1
+
+
+"""
+DON'T MODIFY ANYTHING IN THIS CELL THAT IS BELOW THIS LINE
+"""
+tests.test_con_pool(conv2d_maxpool)
